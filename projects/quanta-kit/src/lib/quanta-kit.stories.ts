075fefa44d8a8a40a2b4f6ba3804171f61c27d84
Marvin Aroza <developertom23@gmail.com>
2025-08-05 15:52:41 +0100

    stories cleanup

--- projects/quanta-kit/src/lib/quanta-kit.stories.ts
+++ projects/quanta-kit/src/lib/quanta-kit.stories.ts
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { fn } from 'storybook/test';
 
-import { ButtonComponent } from './button.component';
+import { QuantaKit } from './quanta-kit';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-const meta: Meta<ButtonComponent> = {
-  title: 'Example/Button',
-  component: ButtonComponent,
+const meta: Meta<QuantaKit> = {
+  title: 'Example/Starter',
+  component: QuantaKit,
   tags: ['autodocs'],
   argTypes: {
     backgroundColor: {
@@ -18,32 +18,8 @@ const meta: Meta<ButtonComponent> = {
 };
 
 export default meta;
-type Story = StoryObj<ButtonComponent>;
+type Story = StoryObj<QuantaKit>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
 };
