e339d2650bda6f5020267556ad58b23b42e63d6e
Marrel07Pinto <marrel7pinto@gmail.com>
2025-09-01 06:49:23 +0100

    feat(QK1): added simple button bookmark and completed and changed the structure

---
+++ packages/quanta-kit-angular/projects/quanta-kit/src/lib/Buttongroup/button-group.stories.ts
@@ -0,0 +1,77 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { fn } from 'storybook/test';
+import { ButtonGroupComponent } from './button-group.component';
+
+const meta: Meta<ButtonGroupComponent> = {
+  title: 'Components/button-group',
+  component: ButtonGroupComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [ButtonGroupComponent],
+    }),
+  ],
+  args: {
+    disabled: false,
+    loading: false,
+    hideTextOnLoading: false,
+    ariaLabel: '',
+    testId: '',
+    clicked: fn(),
+    focused: fn(),
+    blurred: fn(),
+  },
+};
+
+export default meta;
+type Story = StoryObj<ButtonGroupComponent>;
+
+export const Simplebuttons: Story = {
+  args: {
+    variant: 'primary',
+    disabled: false,
+    loading: false,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+    <div style="display: flex; gap: 10px; flex-wrap: wrap; align-items: center; margin-bottom: 20px;">
+    <qk-button-group variant="primary">Primary</qk-button-group>
+    <qk-button-group variant="edit">Edit</qk-button-group>
+    <qk-button-group variant="info">info</qk-button-group>
+    <qk-button-group variant="delete">Delete</qk-button-group>
+    <qk-button-group variant="default">Default</qk-button-group>
+    </div>  
+`,
+  }),
+
+  parameters: {
+    docs: {
+      description: {
+        story: 'Button group',
+      },
+    },
+  },
+};
+
+export const Buttonsizes: Story = {
+  render: () => ({
+    template: `<div>Button Sizes</div>`, // placeholder
+  }),
+};
+
+export const Loadingbuttons: Story = {
+  render: () => ({
+    template: `<qk-button-group variant="primary" [loading]="true">Loading</qk-button-group>`, // placeholder
+  }),
+};
+
+export const Buttonicons: Story = {
+  render: () => ({
+    template: `<qk-button-group variant="success">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="white" viewBox="0 0 24 24">
+    <path d="M20.285 6.709l-11.285 11.292-5.285-5.292 1.414-1.414 3.871 3.878 9.871-9.878z"/>
+  </svg>
+  </qk-button-group>`, 
+  }),
+};
