2eb220c719cfb957afc1c58c4a4961da33d65d32
Marrel07Pinto <marrel7pinto@gmail.com>
2025-10-08 06:49:03 +0100

    feat(AAP13): simple button group

--- packages/quanta-kit-angular/projects/quanta-kit/src/lib/Buttongroup/button-group.component.ts
+++ packages/quanta-kit-angular/projects/quanta-kit/src/lib/Buttongroup/button-group.component.ts
@@ -6,10 +6,10 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './button-group.component.html',
-  styleUrls: ['./button-group.component.scss']
+  styleUrls: ['./button-group.component.scss'],
 })
 export class ButtonGroupComponent {
-  @Input() buttons: { label: string, variant?: string }[] = [];
+  @Input() buttons: { label: string; variant?: string }[] = [];
 
   handleClick(btn: any) {
     alert(`You clicked ${btn.label}`);
